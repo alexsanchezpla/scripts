8bc907a71a12a6065e78fc23384cf03ce881aa2e
Alex <alex@alex-Dell-Precision-T1500.(none)>
2015-10-21 17:06:24 +0200

    Creacio d'un nou arxiu

---
+++ genesFromTopTable.R
@@ -0,0 +1,45 @@
+
+genesFromTopTable <- function (filename, entrezOnly = TRUE, 
+                               adjOrrawP = "adj",
+                               Pcutoff = 0.05,
+                               FCcutoff = 1,
+                               id2Select = "EntrezsA", # c("EntrezsA","SYMBOL", "other")
+                               cols2Select = 2){
+  
+  topTab <- read.csv(file.path(listsDir, filename), head=TRUE, sep=";", dec=",", row.names=1)
+  dim(topTab)
+  colnames(topTab)
+  if (entrezOnly) {
+    selectedEntrez <- !is.na(topTab[,"EntrezsA"])
+    topTab <- topTab[selectedEntrez,]
+  }
+  dim(topTab)
+  if (Pcutoff < 1){
+    if (adjOrrawP=="adj"){
+      selectedP <- topTab[,"adj.P.Val"] < Pcutoff
+    }else{
+      selectedP <- topTab[,"P.Value"] < Pcutoff
+    }
+    topTab<- topTab[selectedP, ]
+  }
+  dim(topTab)
+  if (FCcutoff > 0){
+    selectedFC <-(abs(topTab[,"logFC"]) > FCcutoff)
+    topTab<- topTab[selectedFC, ]
+    dim(topTab)
+  }
+  
+  if (id2Select=="SYMBOL"){
+    geneList <- topTab[,"SYMBOL"]
+  }else{
+    if (id2Select=="Entrez"){
+      geneList <- topTab[,"EntrezsA"]
+    }else{
+      if(cols2Select> 0){
+        geneList <- topTab[,cols2Select]
+      }else{
+        geneList <-rownames(topTab)
+      }}}
+  length(geneList)
+  return(geneList)
+}
