46fffdf13ac5d8d6744d7b0710e877030206be72
alexsanchezpla <asanchez@ub.edu>
2017-05-24 00:54:15 +0200

    Folder 'Exemple_Analysis_with_Bioconductor' has been removed from here and turned into a repository

--- Build_Validate_Classifier/codi R projectes similars/scripts/geometricMeanSD.R
+++
@@ -1,9 +0,0 @@
-geomean <- function(x, na.rm = FALSE, trim = 0, ...)
-{
-  exp(mean(log(x, ...), na.rm = na.rm, trim = trim, ...))
-}
-
-geosd <- function(x, na.rm = FALSE, ...)
-{
-  exp(sd(log(x, ...), na.rm = na.rm, ...))
-}
